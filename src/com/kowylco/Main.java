a32e1964c31633d623d831f4175b75ae0d06bea6
kowlco <ptoxas@gmail.com>
2022-03-19 22:54:31 +0300

    убраны промежуточные выводы; расчеты вынесены в класс Calc; добаавлена проверка на отрицательные римские; добавлен стартовый вывод; применены рекомендаации IDEA;

--- src/com/kowylco/Main.java
+++ src/com/kowylco/Main.java
@@ -3,7 +3,6 @@ package com.kowylco;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
@@ -12,6 +11,14 @@ public class Main {
         String[] inputArray  = new String[3];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inputStr = "";
+
+        System.out.println("""
+                Калькулятор умеет выполнять операции сложения, вычитания, умножения и деления с двумя числами: a + b, a - b, a * b, a / b.
+                Данные передаются в одну строку
+                Калькулятор принимает на вход арабские или римские числа от 1 до 10 включительно, не более.
+                Калькулятор умеет работать только с арабскими или римскими цифрами одновременно
+                Введите выражение""");
+
         try {
             inputStr = br.readLine();
         } catch (IOException e) {
@@ -21,31 +28,31 @@ public class Main {
         inputArray[0] = inputStr.substring(0,inputStr.indexOf(operation));
         inputArray[1] = operation;
         inputArray[2] = inputStr.substring(inputStr.indexOf(operation) + 1);
-        System.out.println(inputStr);
-        System.out.println(Arrays.toString(inputArray));
-        
-        Numbers numbers = new Numbers(inputArray);
+
+        System.out.println(Calc.calculate(inputArray));
+    }
+}
+
+class Calc {
+    public static String calculate(String[] inputArray) throws Exception {
         int intResult;
-        switch (inputArray[1]) {
-            case "+":
-                intResult = numbers.numberOne + numbers.numberTwo;
-                break;
-            case "-":
-                intResult = numbers.numberOne - numbers.numberTwo;
-                break;
-            case "/":
-                intResult = numbers.numberOne / numbers.numberTwo;
-                break;
-            case "*":
-                intResult = numbers.numberOne * numbers.numberTwo;
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + inputArray[1]);
-        }
-        System.out.println(intResult);
+        Numbers numbers = new Numbers(inputArray);
+        intResult = switch (inputArray[1]) {
+            case "+" -> numbers.numberOne + numbers.numberTwo;
+            case "-" -> numbers.numberOne - numbers.numberTwo;
+            case "/" -> numbers.numberOne / numbers.numberTwo;
+            case "*" -> numbers.numberOne * numbers.numberTwo;
+            default -> throw new IllegalStateException("Unexpected value: " + inputArray[1]);
+        };
+
         if (numbers.isRoman) {
-            System.out.println(Numbers.intToRoman(intResult));
-        }
+            if (intResult < 0) {
+                throw new Exception("в римской системе нет отрицательных чисел");
+            } else if (intResult == 0) {
+                return "0";
+            }
+            return Numbers.intToRoman(intResult);
+        } else return Integer.toString(intResult);
     }
 }
 
@@ -54,9 +61,9 @@ class Operation {
         String[] operators = new String[]{"+", "-", "/", "*"};
         int posOperator = -1;
         int sumPosOperators = 0;
-        for (int i = 0; i < operators.length; i++){
-            if (inputStr.contains(operators[i]) && inputStr.indexOf(operators[i]) == inputStr.lastIndexOf(operators[i])) {
-                posOperator = inputStr.indexOf(operators[i]);
+        for (String operator : operators) {
+            if (inputStr.contains(operator) && inputStr.indexOf(operator) == inputStr.lastIndexOf(operator)) {
+                posOperator = inputStr.indexOf(operator);
                 sumPosOperators = sumPosOperators + posOperator;
             }
         }
@@ -68,9 +75,9 @@ class Operation {
         String[] operators = new String[]{"+", "-", "/", "*"};
         int posOperator = -1;
         Operation.isValidOperation(inputStr);
-        for (int i = 0; i < operators.length; i++){
-            if (inputStr.contains(operators[i])) {
-                posOperator = inputStr.indexOf(operators[i]);
+        for (String operator : operators) {
+            if (inputStr.contains(operator)) {
+                posOperator = inputStr.indexOf(operator);
             }
         }
         if (posOperator != -1) {
@@ -83,7 +90,7 @@ class Numbers {
     int numberOne;
     int numberTwo;
     boolean isRoman = false;
-    Numbers(String[] inData) {
+    Numbers(String[] inData) throws Exception {
         try {
             numberOne = Integer.parseInt(inData[0]);
             numberTwo = Integer.parseInt(inData[2]);
@@ -93,44 +100,24 @@ class Numbers {
             numberTwo = this.parseRoman(inData[2]);
             isRoman = true;
         }
+        if (numberOne > 10 || numberTwo >10) {
+            throw new Exception("Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более");
+        }
     }
     public int parseRoman(String inStr) {
-        int outInt;
-        switch (inStr.toUpperCase()) {
-            case "I":
-                outInt = 1;
-                break;
-            case "II":
-                outInt = 2;
-                break;
-            case "III":
-                outInt = 3;
-                break;
-            case "IV":
-                outInt = 4;
-                break;
-            case "V":
-                outInt = 5;
-                break;
-            case "VI":
-                outInt = 6;
-                break;
-            case "VII":
-                outInt = 7;
-                break;
-            case "VIII":
-                outInt = 8;
-                break;
-            case "IX":
-                outInt = 9;
-                break;
-            case "X":
-                outInt = 10;
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + inStr);
-        }
-        return outInt;
+        return switch (inStr.toUpperCase()) {
+            case "I" -> 1;
+            case "II" -> 2;
+            case "III" -> 3;
+            case "IV" -> 4;
+            case "V" -> 5;
+            case "VI" -> 6;
+            case "VII" -> 7;
+            case "VIII" -> 8;
+            case "IX" -> 9;
+            case "X" -> 10;
+            default -> throw new IllegalStateException("Unexpected value: " + inStr);
+        };
     }
     public static String intToRoman(int inInt) {
         RomanNumber[] romanNumbers = RomanNumber.values();
